9dad59294ad829642ba54b7c6a785a24b9a0ff04
Alexander Bahlk <abahlk82@gmail.com>
2025-09-09 15:25:23 -0400

    update to modern docker

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,6 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
-  get "/health", to: proc { [200, {}, ["OK"]] }
-
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
