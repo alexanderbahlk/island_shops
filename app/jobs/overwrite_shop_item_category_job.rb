8aee47e425cc383efd4d1cc7e7d21f691e06b240
Alexander Bahlk <abahlk82@gmail.com>
2025-10-17 19:50:01 -0400

    Create OverwriteShopItemCategoryJob

--- app/jobs/overwrite_shop_item_category_job.rb
+++ app/jobs/overwrite_shop_item_category_job.rb
@@ -1,21 +1,27 @@
-class AssignShopItemCategoryJob < ApplicationJob
+class OverwriteShopItemCategoryJob < ApplicationJob
   queue_as :default
 
-  def perform(batch_size: 10)
-    Rails.logger.info "Starting ShopItem Category assignment job"
+  def perform(old_category_id, batch_size: 10)
+    Rails.logger.info "Starting Overwrite ShopItem Category job for category ID #{old_category_id}"
+
+    #throw error if old_category_id.nil?
+    if old_category_id.nil?
+      Rails.logger.error "Old category ID is nil"
+      return
+    end
 
     processed_count = 0
     matched_count = 0
     error_count = 0
 
-    ShopItem.missing_category.find_in_batches(batch_size: batch_size) do |batch|
+    ShopItem.find_in_batches(category_id: old_category_id, batch_size: batch_size) do |batch|
       batch.each do |shop_item|
         begin
           # Try to find the best matching category
           best_match = ShopItemCategoryMatcher.new(shop_item: shop_item).find_best_match()
 
           if best_match
-            shop_item.update!(category: best_match)
+            shop_item.update!(category: best_match, approved: false)
             matched_count += 1
             Rails.logger.info "Assigned '#{best_match.title}' to '#{shop_item.title}'"
           end
