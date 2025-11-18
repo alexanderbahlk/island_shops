2870ab58212e5c945be1500c64959cd6b78d2505
Alexander Bahlk <abahlk82@gmail.com>
2025-11-18 10:41:08 -0400

    Update Priority view

--- app/admin/priority_shop_items.rb
+++ app/admin/priority_shop_items.rb
@@ -1,24 +1,34 @@
-ActiveAdmin.register_page "Priority Shop Items" do
-  menu parent: "Shop Items", label: "Priority Shop Items"
+ActiveAdmin.register_page 'Priority Shop Items' do
+  menu parent: 'Shop Items', label: 'Priority Shop Items'
 
-  content title: "Priority Shop Items" do
-    panel "Products with high Shop Item Count" do
+  content title: 'Priority Shop Items' do
+    panel 'Products with high Shop Item Count' do
       products = Category.products
       priority_shop_items = []
       products.each do |product|
-        count = product.shop_items.pending_approval.count
-        if count > 1
-          priority_shop_items << { product: product, count: count }
-        end
+        pending_approval_count = product.shop_items.pending_approval.count
+        next unless pending_approval_count > 1
+
+        approved_count = product.shop_items.approved.count
+        all_count = product.shop_items.all.count
+        to_approve_ratio = if all_count.positive?
+                             pending_approval_count.to_f / all_count.to_f
+                           else
+                             0
+                           end
+        priority_shop_items << { product: product, pending_approval_count: pending_approval_count,
+                                 approved_count: approved_count, all_count: all_count, to_approve_ratio: to_approve_ratio }
       end
-      priority_shop_items.sort_by! { |item| -item[:count] }
-      ul class: "priority-shop_item-list" do
+      priority_shop_items.sort_by! { |item| -item[:pending_approval_count] }
+      ul class: 'priority-shop_item-list' do
         priority_shop_items.each do |item|
-          li class: "priority-shop_item-list-item" do
-            span "(#{item[:product].id}) #{item[:product].title} - #{item[:count]}"
-            text_node link_to "View Shop Items", admin_shop_items_path(q: { category_id_eq: item[:product].id, status_eq: "pending_approval" }), style: "margin-left: 10px;"
-            text_node " | "
-            text_node link_to "Start Overwrite Job", admin_priority_shop_items_start_overwrite_job_path(old_category_id: item[:product].id), method: :post, data: { confirm: "Are you sure you want to start the AssignShopItemCategoryJob for this product?" }, style: "margin-left: 10px;"
+          li class: 'priority-shop_item-list-item' do
+            span "(Id: #{item[:product].id}) #{item[:product].title} - #{item[:pending_approval_count]} from #{item[:all_count]} shop items pending approval (#{(item[:to_approve_ratio] * 100).round(2)}% to approve)"
+            text_node link_to 'View Shop Items',
+                              admin_shop_items_path(q: { category_id_eq: item[:product].id, status_eq: 'pending_approval' }), style: 'margin-left: 10px;'
+            text_node ' | '
+            text_node link_to 'Start Overwrite Job',
+                              admin_priority_shop_items_start_overwrite_job_path(old_category_id: item[:product].id), method: :post, data: { confirm: 'Are you sure you want to start the AssignShopItemCategoryJob for this product?' }, style: 'margin-left: 10px;'
           end
         end
       end
@@ -33,7 +43,7 @@ ActiveAdmin.register_page "Priority Shop Items" do
       AssignShopItemCategoryJob.perform_later(category_id: old_category_id)
       flash[:notice] = "AssignShopItemCategoryJob has been started for category ID #{old_category_id}."
     else
-      flash[:error] = "Invalid parameters. Job could not be started."
+      flash[:error] = 'Invalid parameters. Job could not be started.'
     end
 
     redirect_to admin_priority_shop_items_path
